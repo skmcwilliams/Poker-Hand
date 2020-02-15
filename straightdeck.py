8d786ed886d52f3e876144fbe76cdd043b5b2fce
SKM <skm@Stevens-MacBook-Air.local>
2020-02-14 21:27:47 -0600

    code update

--- straightdeck.py
+++ straightdeck.py
@@ -8,6 +8,7 @@ def straight_hand():
     ranks[-2] = 13
     ranks[-3] = 12
     ranks[-4] = 11
+    ranks = [int(i) for i in ranks]
     
     deck = [(r, s) for s in suits for r in ranks ]
 
@@ -16,7 +17,7 @@ def straight_hand():
     cards = [hand[0][0], hand[1][0], hand[2][0], hand[3][0], hand[4][0]]
     cards = [int(card) for card in cards]
     cards.sort()
-    if cards[4] - cards[3] ==1 and cards[3] - cards[2] == 1 and cards [2] - cards[1] == 1 and cards[1] - cards[0] == 1:
+    if cards[4] - cards[1] == 1 and cards[3] - cards[2] == 1 and cards [2] - cards[1] == 1 and cards[1] - cards[0] == 1:
         print(True)
         print('You have a straight!!')
     else:
